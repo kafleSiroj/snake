9a99852b6963c9d6cf57e70e3a4ba42dafb3b8c1
Siroj K <167022994+kafleSiroj@users.noreply.github.com>
2025-01-25 15:44:51 +0545

    update colors

--- human_mode/colors.py
+++ human_mode/colors.py
@@ -1,6 +1,6 @@
 class Colors:
     WHITE = (225,225,225)
-    RED = (200,0,0)
-    BLACK = (0,0,0)
-    BLUE = (0,0,255)
-    LIGHTBLUE = (0,130,250)
+    RED = (237, 31, 17)
+    BLACK = (39, 38, 41)
+    BLUE = (64, 29, 171)
+    LIGHTBLUE = (71, 111, 186)
