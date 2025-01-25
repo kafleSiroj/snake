be41e16c20acd8d809b055af6972f8ff33fcbb87
Siroj K <167022994+kafleSiroj@users.noreply.github.com>
2025-01-25 18:09:09 +0545

    color update

--- environment/Colors.py
+++ environment/Colors.py
@@ -1,6 +1,6 @@
 class Colors:
     WHITE = (225,225,225)
     RED = (237, 31, 17)
-    BLACK = (0,0,0)
-    BLUE = (62, 42, 122)
+    BLACK = (39, 38, 41)
+    BLUE = (64, 29, 171)
     LIGHTBLUE = (71, 111, 186)
